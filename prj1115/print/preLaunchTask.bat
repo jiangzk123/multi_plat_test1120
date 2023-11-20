d4593af0fbaddbfee3ba728c380654c7244056ec
jiangzk <840948886@qq.com>
2023-11-20 16:53:11 +0800

    添加工作区

--- prj1115/print/preLaunchTask.bat
+++ prj1115/print/preLaunchTask.bat
@@ -1,7 +1,9 @@
 rem ******************start PreLaunchTasks******************
 rem ******************start buildDynamicLib******************
+cmake -G "MinGW Makefiles"  ../lib/addlib/CMakeLists.txt  -B ../lib/addlib/build
 cmake --build ../lib/addlib/build
 rem ******************start buildMain******************
+cmake -G "MinGW Makefiles" ./CMakeLists.txt -B  ./build
 cmake --build ./build
 rem ******************start copyFiles******************
 xcopy /Y /I ".\..\lib\addlib\build\libadd.dll"  ".\build"
