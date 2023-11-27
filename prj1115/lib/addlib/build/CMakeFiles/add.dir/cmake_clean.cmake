e764c8cd1b7a94a9d96f8a27b0a844feb22e87b3
jiangzk123 <840948886@qq.com>
2023-11-27 14:51:59 +0800

    提交补充文件

---
+++ prj1115/lib/addlib/build/CMakeFiles/add.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/add.dir/src/add.cpp.o"
+  "libadd.pdb"
+  "libadd.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/add.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
